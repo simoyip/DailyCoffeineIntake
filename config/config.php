4ed16c643dc9afed12345d0798af8bd905782f4e
simoyip <84817455+simoyip@users.noreply.github.com>
2021-10-11 19:31:06 -0700

    Update config.php

--- config/config.php
+++ config/config.php
@@ -1,1 +1,5 @@
-
+<?php
+  $site_title="Daily Coffeine Intake Planner";  //the title of the web application
+  $site_path="./";                              //Path to the site
+  $drinks_data_filename="data.php";             //drinks data file name
+?>
