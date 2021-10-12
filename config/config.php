a5e25fec69ad1c142b86b4c84499bd0f0b1690ef
simoyip <84817455+simoyip@users.noreply.github.com>
2021-10-12 15:46:20 -0700

    Delete config directory

--- config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-  $site_title="Daily Coffeine Intake Planner";  //the title of the web application
-  $site_path="./";                              //Path to the site
-  $drinks_data_filename="data.php";             //drinks data file name
-?>
