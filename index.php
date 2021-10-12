cc328b574146e80e615652b36710ae4c244f5af8
simoyip <84817455+simoyip@users.noreply.github.com>
2021-10-11 19:32:57 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
+<?php include("includes/config.php");?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("includes/head-tag-contents.php");?>
+</head>
+<body>
 
+<?php include("includes/header.php");?>
+<?php include("includes/navigation.php");?>
+
+<div class="container" id="main-content">
+</div>
+
+<?php include("includes/footer.php");?>
+
+</body>
+</html>
